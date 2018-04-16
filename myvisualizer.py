86d7dacf2b9680898f26d2d0ec31975ca8f9e87b
angwhen <angelmwen@gmail.com>
2018-04-15 21:25:39 -0400

    just copied visualizing code over here

---
+++ myvisualizer.py
@@ -0,0 +1,110 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt #even tho idk about drawing bounding here
+# perhaps can just get good perspective, and open image in matlab to draw ... ?
+from math import sin, cos
+from matplotlib.widgets import Slider, Button, RadioButtons
+
+hz = 0
+hy = 0
+hx = 0
+theta = 0
+
+'''store video properties in a class to have unified access '''
+class Video_Properties:
+    height = 0
+    width = 0
+    time_range = []
+    def __init__(self, height,width,time_range):
+        self.height = height+1 #idk really if this is right ... 
+        self.width = width+1 
+        self.time_range = time_range
+
+""" proj_func are user defined functions to calculate x' and y' on dataframe """        
+def proj_func_x(x,y,t):
+    global hz, hy, hx, theta
+    xprime = x - t*(hx + (hz+1)*(cos(theta)*x-sin(theta)*y) -x)
+    return xprime
+
+def proj_func_y(x,y,t):
+    global hz, hy, hx, theta
+    yprime = y - t*(hy + (hz+1)*(cos(theta)*x+sin(theta)*y) -y)
+    return yprime
+
+def display_frames_stack(df,my_vid_properties):
+    global l, ax,fig,hx,hy,theta,hz
+    mint = my_vid_properties.time_range[0]
+    maxt = my_vid_properties.time_range[1000]
+    mytime_df = df.loc[(df["time"] >= mint) & (df["time"] <= maxt)]
+    mytime_df['x_prime'] = mytime_df.apply(lambda d: proj_func_x(d['x'],d['y'],d['time']),axis=1)
+    mytime_df['y_prime'] = mytime_df.apply(lambda d: proj_func_y(d['x'],d['y'],d['time']),axis=1)
+    print mytime_df
+    return mytime_df
+    
+def update(val):
+    global hz, hy, hx, theta
+    hx = hx_slider.val
+    hy = hy_slider.val
+    hz = hz_slider.val
+    theta = theta_slider.val
+
+    mytime_df = display_frames_stack(df,my_vid_properties)
+    #print(mytime_df)
+    x = mytime_df["x_prime"].tolist()
+    y = mytime_df["y_prime"].tolist()
+    xx = np.vstack((x,y))
+    l.set_offsets(xx.T) 
+    fig.canvas.draw_idle()
+    
+    
+def main():
+    # reading/setting up data
+    #df = pd.read_csv("datasets/yosemite.txt", sep =" ",header=None)
+    chunksize = 10 ** 6
+    i = 0
+    for chunk in pd.read_csv("events.txt", sep = " ", header=None, chunksize=chunksize):
+        i += 1
+        if i ==2:
+            break
+    df = chunk
+    
+    df.columns = ["time","x","y","light_change"]
+    df = df.drop(labels="light_change",axis=1) #remove light change data for now
+    
+    height = df['y'].max() # this is not necessarily correct .... but how else to tell?
+    width = df['x'].max()
+    time_range = df["time"].unique()
+    my_vid_properties = Video_Properties(height,width,time_range)
+
+    #showing layers of frames with dif parameters
+    global hz, hy, hx, theta
+    # i really dont think these globals are a good idea but whatever i guess
+    global fig, ax, hx_slider, hy_slider,hz_slider,theta_slider, df, my_vid_properties, l 
+
+    fig,ax = plt.subplots()
+    fig.subplots_adjust(left=0.25, bottom=0.25)
+    axis_color = 'lightgoldenrodyellow'
+    
+    # make sliders
+    hx_slider_ax  = fig.add_axes([0.25, 0.2, 0.65, 0.03], axisbg=axis_color)
+    hy_slider_ax = fig.add_axes([0.25, 0.15, 0.65, 0.03], axisbg=axis_color)
+    hz_slider_ax  = fig.add_axes([0.25, 0.1, 0.65, 0.03], axisbg=axis_color)
+    theta_slider_ax = fig.add_axes([0.25, 0.05, 0.65, 0.03], axisbg=axis_color)
+    hx_slider = Slider(hx_slider_ax, 'hx', -1000, 1000.0, valinit=hx)
+    hy_slider = Slider(hy_slider_ax, 'hy', -1000, 1000.0, valinit=hy)
+    hz_slider = Slider(hz_slider_ax, 'hz', -0.1, 100.0, valinit=hz)
+    theta_slider = Slider(theta_slider_ax, 'theta', -0.1, 7.0, valinit=theta)
+    hx_slider.on_changed(update)
+    hy_slider.on_changed(update)
+    hz_slider.on_changed(update)
+    theta_slider.on_changed(update)
+    mytime_df = display_frames_stack(df,my_vid_properties)
+    l = ax.scatter(mytime_df["x_prime"],mytime_df["y_prime"],c=mytime_df["time"])
+    ax.set_xlim = my_vid_properties.width*3
+    ax.set_ylim = my_vid_properties.height*time_range[-1]
+    plt.show()
+
+    
+if __name__ == "__main__":
+    main()
+    
