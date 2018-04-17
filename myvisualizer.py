f2e4b36ae1f6e4fd726aec3aa1c4dd8abff11db7
angwhen <angelmwen@gmail.com>
2018-04-16 22:28:14 -0400

    have options for start and end time now ... but still really slow to spin around if a lot of pts

--- myvisualizer.py
+++ myvisualizer.py
@@ -18,6 +18,8 @@ class Video_Properties:
        
 class MyVisualizer:
     param_dict = {"hx":0, "hy":0, "hz":0, "theta":0}
+    mint_ind = 0
+    maxt_ind = 100
     my_vid_properties = None
     df = None
     hx_slider = None
@@ -28,7 +30,13 @@ class MyVisualizer:
     ax = None
     my_scatter = None
 
-    def __init__(self,df):
+    def __init__(self,df,param_dict = None, mint_ind = 0, maxt_ind=100):
+        if param_dict != None:
+            self.param_dict = param_dict
+
+        self.mint_ind = mint_ind
+        self.maxt_ind = maxt_ind
+        
         # making my_vid_properties
         height = df['y'].max()
         width = df['x'].max()
@@ -51,9 +59,9 @@ class MyVisualizer:
         hz_slider_ax  = self.fig.add_axes([0.25, 0.1, 0.65, 0.03], axisbg=axis_color)
         theta_slider_ax = self.fig.add_axes([0.25, 0.05, 0.65, 0.03], axisbg=axis_color)
         # define start and end etc
-        self.hx_slider = Slider(hx_slider_ax, 'hx', -1000, 1000.0, valinit=self.param_dict["hx"])
-        self.hy_slider = Slider(hy_slider_ax, 'hy', -1000, 1000.0, valinit=self.param_dict["hy"])
-        self.hz_slider = Slider(hz_slider_ax, 'hz', -0.1, 100.0, valinit=self.param_dict["hz"])
+        self.hx_slider = Slider(hx_slider_ax, 'hx', -10.0, 10.0, valinit=self.param_dict["hx"])
+        self.hy_slider = Slider(hy_slider_ax, 'hy', -50.0, 50.0, valinit=self.param_dict["hy"])
+        self.hz_slider = Slider(hz_slider_ax, 'hz', -50.0, 50.0, valinit=self.param_dict["hz"])
         self.theta_slider = Slider(theta_slider_ax, 'theta', -0.1, 7.0, valinit=self.param_dict["theta"])
         # update graph when slider changes
         self.hx_slider.on_changed(self.update)
@@ -62,7 +70,7 @@ class MyVisualizer:
         self.theta_slider.on_changed(self.update)
 
         # display original scatter plot
-        mytime_df = self.display_frames_stack()
+        mytime_df = self.get_event_cloud()
         self.my_scatter = self.ax.scatter(mytime_df["x_prime"],mytime_df["y_prime"],c=mytime_df["time"])
         plt.show() 
         
@@ -76,9 +84,9 @@ class MyVisualizer:
         return yprime
 
     """ gets the projection coordinates of all points in our time range """
-    def display_frames_stack(self):
-        mint = self.my_vid_properties.time_range[0]
-        maxt = self.my_vid_properties.time_range[1000]
+    def get_event_cloud(self):
+        mint = self.my_vid_properties.time_range[self.mint_ind]
+        maxt = self.my_vid_properties.time_range[self.maxt_ind]
         mytime_df = self.df.loc[(self.df["time"] >= mint) & (self.df["time"] <= maxt)]
         mytime_df['x_prime'] = mytime_df.apply(lambda d: self.proj_func_x(d['x'],d['y'],d['eq_sep_time']),axis=1)
         mytime_df['y_prime'] = mytime_df.apply(lambda d: self.proj_func_y(d['x'],d['y'],d['eq_sep_time']),axis=1)
@@ -88,16 +96,15 @@ class MyVisualizer:
         # allow us to always be in bounds,showing good area
         w = self.my_vid_properties.width
         h = self.my_vid_properties.height
-        tmin = 0 
-        tmax = 1000 #make this variable
         x_list = [0,0,w,w,0,  0,0,w,w,0]
         y_list = [0,h,h,0,0,  0,h,h,0,0]
-        t_list = [tmin,tmin,tmin,tmin,tmin, tmax,tmax,tmax,tmax,tmax]
+        t_list = [self.mint_ind,self.mint_ind,self.mint_ind,self.mint_ind,self.mint_ind,
+                  self.maxt_ind,self.maxt_ind,self.maxt_ind,self.maxt_ind,self.maxt_ind,]
         xproj_list = [self.proj_func_x(x,y,t) for x,y,t in zip(x_list,y_list,t_list)]
         yproj_list = [self.proj_func_y(x,y,t) for x,y,t in zip(x_list,y_list,t_list)]
         return xproj_list,yproj_list
 
-    def update(self,val):
+    def update(self, val):
         self.param_dict["hx"] = self.hx_slider.val
         self.param_dict["hy"] = self.hy_slider.val
         self.param_dict["hz"] = self.hz_slider.val
@@ -109,7 +116,7 @@ class MyVisualizer:
         self.ax.set_xlim([min(xproj_list),max(xproj_list)])
 
         #display other perspective
-        mytime_df = self.display_frames_stack()
+        mytime_df = self.get_event_cloud()
         x = mytime_df["x_prime"].tolist()
         y = mytime_df["y_prime"].tolist()
         xx = np.vstack((x,y))
@@ -120,25 +127,33 @@ class MyVisualizer:
 def main():
     # reading/setting up data
     chunksize = 10 ** 6
-    for chunk in pd.read_csv("events.txt", sep = " ", header=None, chunksize=chunksize):
+    for chunk in pd.read_csv("yosemite.txt", sep = " ", header=None, chunksize=chunksize):
         break
     df = chunk
-
-    # do optparse things
-    start_time_ind  = 0
-    end_time_ind = 50
-    parser = OptionParser()
-    parser.add_option("-s","--start",action="store", type ="int",dest = "start_time_ind")
-    (options,args) = parser.parse_args(sys.argv[1:])
     
     # putting data into format we'd like
     df.columns = ["time","x","y","light_change"]
     df = df.drop(labels="light_change",axis=1) 
     df = df.assign(eq_sep_time=(df['time']).astype('category').cat.codes) #make new column id to tell time in constant growing manner
 
-    my_visualizer = MyVisualizer(df)
-    
 
+    # do optparse things
+    start_time_ind  = 0
+    end_time_ind = 50
+    parser = OptionParser()
+    parser.add_option("-s","--start_ind",action="store", type ="int",dest = "mint_ind")
+    parser.add_option("-e","--end_ind",action="store", type ="int",dest = "maxt_ind")
+    (options,args) = parser.parse_args(sys.argv[1:])
+    mint_ind = options.mint_ind
+    maxt_ind = options.maxt_ind
+    if mint_ind == None:
+        mint_ind = 0
+    if maxt_ind == None:
+        maxt_ind = 50
+    print mint_ind
+    print maxt_ind
+    
+    my_visualizer = MyVisualizer(df,param_dict = {"hx":0, "hy":0, "hz":0, "theta":0}, mint_ind = mint_ind,maxt_ind = maxt_ind)
     
 if __name__ == "__main__":
     main()
